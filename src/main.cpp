3169881494426abd55ce0f260294bf175ead342b
Christian Göhring <cg@2601.me>
2021-04-18 21:15:16 +0200

    error handling

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,24 @@ int
 main(int argc, char *argv[]) {
     if (argc < 2) {
         std::cerr << "dog: missing input file!\n";
+        std::cerr << "usage: dog <input_file> [-n]\n";
         return -1;
     }
 
-    std::ifstream file(argv[1], std::ios::in);
-    std::string   line;
-    int           line_count = 1;
-    while (std::getline(file, line)) {
+    std::ifstream input_file(argv[1], std::ios::in);
+    if (!input_file.is_open()) {
+        std::cerr << "dog: could not open input file '" << argv[1] << "'!\n";
+        return -1;
+    }
+
+    std::string line;
+    int         line_count = 1;
+    while (std::getline(input_file, line)) {
         if (argc >= 3 && argv[2] == std::string("-n"))
             std::cout << std::setw(5) << std::setfill(' ') << line_count++ << "  ";
 
         std::cout << line << '\n';
     }
+
+    return 0;
 }
