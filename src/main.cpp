f1f9d9382d3f81d4df9efa10e90f7986486871a0
Christian Göhring <cg@2601.me>
2021-10-19 00:28:24 +0200

    fix for quick-and-dirty

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -9,25 +10,31 @@ main(int argc, char *argv[]) {
     std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
 #endif
 
-    if (argc < 2) {
+    const bool show_line_numbers = argc >= 2 && (strcmp(argv[1], "-n") == 0);
+    const int  input_files_i     = show_line_numbers ? 2 : 1;
+
+    if (argc < input_files_i) {
         std::cerr << "dog: missing input file!\n";
-        std::cerr << "usage: dog <input_file> [-n]\n";
+        std::cerr << "usage: dog [-n] <input_file> ...\n";
         return EXIT_FAILURE;
     }
 
-    std::ifstream input_file(argv[1], std::ios::in);
-    if (!input_file.is_open()) {
-        std::cerr << "dog: could not open input file '" << argv[1] << "'!\n";
-        return EXIT_FAILURE;
-    }
+    for (auto i = input_files_i; i < argc; ++i) {
+        std::ifstream input_file(argv[i], std::ios::in);
+        if (!input_file.is_open()) {
+            std::cerr << "dog: could not open input file '" << argv[i] << "'!\n";
+            return EXIT_FAILURE;
+        }
 
-    std::string line;
-    int         line_count = 1;
-    while (std::getline(input_file, line)) {
-        if (argc >= 3 && strcmp(argv[2], "-n") == 0)
-            std::cout << std::setw(5) << std::setfill(' ') << line_count++ << "  ";
+        std::string line;
+        int         line_count = 1;
+        while (std::getline(input_file, line)) {
+            if (show_line_numbers) {
+                std::cout << std::setw(6) << std::setfill(' ') << line_count++ << "  ";
+            }
 
-        std::cout << line << '\n';
+            std::cout << line << '\n';
+        }
     }
 
     return EXIT_SUCCESS;
