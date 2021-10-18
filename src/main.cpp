bd44175d1b37f9ff5bfe288aab23e13b1d085265
Christian Göhring <cg@2601.me>
2021-10-19 00:13:32 +0200

    diy method implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,41 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+
+#include "program_options.hpp"
 
 int
 main(int argc, char *argv[]) {
-#ifdef _DEBUG
-    std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
-#endif
-
-    if (argc < 2) {
-        std::cerr << "dog: missing input file!\n";
-        std::cerr << "usage: dog <input_file> [-n]\n";
+    try {
+        program_options::parse(argc, argv);
+    } catch (const std::exception &x) {
+        std::cerr << x.what() << '\n';
+        std::cerr << "usage: dog [-n|--number] [-E|--show-ends] <input_file> ...\n";
         return EXIT_FAILURE;
     }
 
-    std::ifstream input_file(argv[1], std::ios::in);
-    if (!input_file.is_open()) {
-        std::cerr << "dog: could not open input file '" << argv[1] << "'!\n";
-        return EXIT_FAILURE;
-    }
+    for (const auto &file_name : program_options::input_files()) {
+        std::ifstream input_file(file_name, std::ios::in);
+        if (!input_file.is_open()) {
+            std::cerr << "dog: could not open input file '" << file_name << "'!\n";
+            return EXIT_FAILURE;
+        }
 
-    std::string line;
-    int         line_count = 1;
-    while (std::getline(input_file, line)) {
-        if (argc >= 3 && strcmp(argv[2], "-n") == 0)
-            std::cout << std::setw(5) << std::setfill(' ') << line_count++ << "  ";
+        std::string line;
+        int         line_count = 1;
+        while (std::getline(input_file, line)) {
+            if (program_options::show_line_numbers()) {
+                std::cout << std::setw(6) << std::setfill(' ') << line_count++ << "  ";
+            }
 
-        std::cout << line << '\n';
+            std::cout << line;
+
+            if (program_options::show_ends()) {
+                std::cout << '$';
+            }
+
+            std::cout << '\n';
+        }
     }
 
     return EXIT_SUCCESS;
--- src/program_options.cpp
+++ src/program_options.cpp
@@ -1,33 +1,58 @@
 #include "program_options.hpp"
 
+#include <filesystem>
 #include <stdexcept>
-#include <string>
 #include <vector>
+
+namespace {
+
+static std::vector<std::string_view> _input_files;
+static bool                          _show_line_numbers = false;
+static bool                          _show_ends         = false;
+
+}  // namespace
 
 void
 program_options::parse(int argc, char* argv[]) {
     const std::vector<std::string_view> args(argv + 1, argv + argc);
 
-    bool first = false;
     for (const auto& arg : args) {
-        if (!first) {
-            _input_file = arg;
-            first       = true;
-        } else if (arg == "-n") {
-            if (_show_line_numbers) throw std::runtime_error("dog: cannot use -n parameter twice!");
-            _show_line_numbers = true;
-        } else {
-            throw std::runtime_error("dog: unknown parameter!");
+        if (_input_files.empty()) {
+            if (arg == "-n" || arg == "--number") {
+                if (_show_line_numbers) {
+                    throw std::runtime_error("dog: cannot use -n/--number parameter twice!");
+                }
+                _show_line_numbers = true;
+                continue;
+            }
+
+            if (arg == "-E" || arg == "--show-ends") {
+                if (_show_ends) {
+                    throw std::runtime_error("dog: cannot use -E/--show-ends parameter twice!");
+                }
+                _show_ends = true;
+                continue;
+            }
         }
+
+        if (!std::filesystem::exists(arg)) {
+            throw std::runtime_error(std::string("dog: ") + std::string(arg) + ": No such file or directory");
+        }
+        _input_files.push_back(arg);
     }
 }
 
-std::string_view
-program_options::input_file() const noexcept {
-    return _input_file;
+const std::vector<std::string_view>&
+program_options::input_files() {
+    return _input_files;
 }
 
 bool
-program_options::show_line_numbers() const noexcept {
+program_options::show_ends() {
+    return _show_ends;
+}
+
+bool
+program_options::show_line_numbers() {
     return _show_line_numbers;
 }
