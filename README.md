be019c90b677b8fd9a34e2bc04de48ac7aad910d
Christian Göhring <cg@2601.me>
2021-10-19 00:49:56 +0200

    diy-method with only input file arguments

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# dog: a cat-like cli tool 🐕 
+# dog: a cat-like cli tool 🐕
 
 You probably heard of and used [cat](https://www.man7.org/linux/man-pages/man1/cat.1.html) in Linux. Now be ready for
-the arrival of dog! A command line tool similar to cat to showcase three different methods of command line argument
-parsing in C++.
+the arrival of dog! A toy-example command line tool similar to cat to showcase three different methods of command line
+argument parsing in C++.
 
 I wrote a blog article about it on Medium or here on [mostsignificant.github.io](https://mostsignificant.github.io)
 where you can follow along. The three different methods reside on different branches. The main branch is reserved for
--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,13 @@ main(int argc, char *argv[]) {
     std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
 #endif
 
-    const bool show_line_numbers = argc >= 2 && (strcmp(argv[1], "-n") == 0);
-    const int  input_files_i     = show_line_numbers ? 2 : 1;
-
-    if (argc < input_files_i) {
+    if (argc < 2) {
         std::cerr << "dog: missing input file!\n";
-        std::cerr << "usage: dog [-n] <input_file> ...\n";
+        std::cerr << "usage: dog <input_file> ...\n";
         return EXIT_FAILURE;
     }
 
-    for (auto i = input_files_i; i < argc; ++i) {
+    for (auto i = 1; i < argc; ++i) {
         std::ifstream input_file(argv[i], std::ios::in);
         if (!input_file.is_open()) {
             std::cerr << "dog: could not open input file '" << argv[i] << "'!\n";
@@ -27,12 +24,7 @@ main(int argc, char *argv[]) {
         }
 
         std::string line;
-        int         line_count = 1;
         while (std::getline(input_file, line)) {
-            if (show_line_numbers) {
-                std::cout << std::setw(6) << std::setfill(' ') << line_count++ << "  ";
-            }
-
             std::cout << line << '\n';
         }
     }
